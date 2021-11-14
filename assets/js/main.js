acca20ac3eeaa376157c53ea604faf8b90241a15
Mathieu-C <chatillon.mathieu@gmail.com>
2021-11-14 18:12:58 +0100

    Add rating submit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,28 @@ const modalContainer = document.getElementById("review-modal");
 const toggleModal = () => {
   modalContainer.classList.toggle("review-modal--is-open");
 }
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const data = {
+    rating : form.querySelector('input[name="rating"]:checked').value,
+    body : form.querySelector('textarea').value,
+  }
+
+  fetch(
+    `${API_ROOT}/reviews`,
+    {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status === "SUCCESS") {
+        toggleModal();
+      }
+    })
+}
